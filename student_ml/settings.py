ab2ca96c1d2cfc949ab8e0e9278d64ec290cbb61
HATUMA Charles <hatumacharles1@gmail.com>
2026-01-27 08:43:31 +0200

    feat: configure for Render hosting

--- student_ml/settings.py
+++ student_ml/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-+0(h^t1f!u%xz143jyj@2d#4kxd+11$858emq+5kl=evb%g)@m
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"] # Adjust this to your Render URL later
 
 
 # Application definition
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -118,3 +119,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
+STORAGES = {
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
